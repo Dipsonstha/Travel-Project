b8b67ebaaa1d4cf6a7098dfc1adb1b453d25d53d
Dipson <Dipson9861@gmail.com>
2023-05-28 18:07:06 +0545

    Added book_form.php with some function but not working

---
+++ book_form.php
@@ -0,0 +1,60 @@
+<?php
+
+    session_start();
+
+$connection = mysqli_connect('localhost', 'root', '', 'book_db');
+if (isset($_POST['send'])) {
+
+    // Check if the form has already been submitted
+    if (isset($_SESSION['form_submitted']) && $_SESSION['form_submitted']) {
+        echo 'The form has already been submitted.';
+        exit();
+    }
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $location = $_POST['location'];
+    $guests = $_POST['guests'];
+    $arrivals = $_POST['arrivals'];
+    $leaving = $_POST['leaving'];
+
+    // Perform form validation
+    if (!empty($name) && !empty($email) && !empty($phone) && !empty($address) && !empty($location) && !empty($guests) && !empty($arrivals) && !empty($leaving)) {
+        // All required fields have a value, proceed with database insertion
+
+        // Escape special characters to prevent SQL injection
+        $name = mysqli_real_escape_string($connection, $name);
+        $email = mysqli_real_escape_string($connection, $email);
+        $phone = mysqli_real_escape_string($connection, $phone);
+        $address = mysqli_real_escape_string($connection, $address);
+        $location = mysqli_real_escape_string($connection, $location);
+        $guests = mysqli_real_escape_string($connection, $guests);
+        $arrivals = mysqli_real_escape_string($connection, $arrivals);
+        $leaving = mysqli_real_escape_string($connection, $leaving);
+
+        // Create the SQL query
+        $request = "INSERT INTO book_form (name, email, phone, address, location, guests, arrivals, leaving) 
+                    VALUES ('$name', '$email', '$phone', '$address', '$location', '$guests', '$arrivals', '$leaving')";
+
+        // Execute the query
+        mysqli_query($connection, $request);
+
+
+        // Set the form submitted status in session
+        $_SESSION['form_submitted'] = true;
+
+
+        // Redirect to the book.php page
+        header('location: book.php');
+        exit();
+    } else {
+        // Some required fields are empty, display an error message
+        echo 'Please fill in all required fields.';
+    }
+} else {
+    echo 'Something went wrong. Please try again.';
+}
+
+?>
